9bf2e82a30c187e4249da49e3c48351fe35d2d3e
HabArt <105586654+HabArt1978@users.noreply.github.com>
2025-07-15 04:04:50 +0300

    1 - project init and setting

---
+++ memo.md
@@ -0,0 +1,14 @@
+## Создание проекта
+
+```sh
+
+laravel new repeat-and-test-laravel
+php artisan sail:install
+sail up -d
+sail artisan migrate
+sail npm install
+sail down
+sail up -d
+sail npm run dev
+
+```
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Доска задач</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class='text-red-600'>Home Page</h1>
+</body>
+
+</html>
