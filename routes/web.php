27f795e695bf909b5c605f7da8457c568ff14092
HabArt <105586654+HabArt1978@users.noreply.github.com>
2025-08-19 16:18:52 +0300

    16 - Input @csrf, Create job.

--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,33 @@ Route::get('/contacts', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(12);
+    $jobs = Job::with('employer')->latest()->paginate(12);
 
     // $jobs = Job::with('employer')->simplePaginate(12);
     // $jobs = Job::with('employer')->cursorPaginate(12);
 
-    return view('jobs', ['jobs' => $jobs]);
+    return view('jobs.index', ['jobs' => $jobs]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    // dd(request()->all());
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
 });
