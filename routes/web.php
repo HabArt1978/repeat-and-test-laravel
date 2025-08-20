f18f95556f1b956670af1f1b1b5f8bd2323c37df
HabArt <105586654+HabArt1978@users.noreply.github.com>
2025-08-20 22:59:52 +0300

    21 - Route Model Binding, finish route refactoring

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,18 @@ Route::view('/', 'home', ['greeting' => 'Привет', 'name' => 'Артур'])
 Route::view('/about', 'about');
 Route::view('/contacts', 'contacts');
 
+Route::resource('jobs', JobController::class);
+
 //========= Jobs CRUD =========//
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', [JobController::class, 'index']);
-    Route::get('/jobs/create', [JobController::class, 'create']);
-    Route::get('/jobs/{job}', [JobController::class, 'show']);
-    Route::post('/jobs', [JobController::class, 'store']);
-    Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-    Route::patch('/jobs/{job}', [JobController::class, 'update']);
-    Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-});
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', [JobController::class, 'index']);
+//     Route::get('/jobs/create', [JobController::class, 'create']);
+//     Route::get('/jobs/{job}', [JobController::class, 'show']);
+//     Route::post('/jobs', [JobController::class, 'store']);
+//     Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+//     Route::patch('/jobs/{job}', [JobController::class, 'update']);
+//     Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+// });
 
 
 
